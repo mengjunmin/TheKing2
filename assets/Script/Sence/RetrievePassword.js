949bc593cc92d1f06f1c98a431271b15f578852a
蒙俊民 <mengjunmin@mengjunmindeMacBook-Pro.local>
2018-09-13 00:05:02 +0800

    retrievepassword

--- assets/Script/Sence/RetrievePassword.js
+++ assets/Script/Sence/RetrievePassword.js
@@ -7,258 +7,244 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        notePrefab:{
-        	default:null,
-        	type:cc.Prefab
+        notePrefab: {
+            default: null,
+            type: cc.Prefab
         },
-        nameListPrefab:{
-            default:null,
-            type:cc.Prefab
+        nameListPrefab: {
+            default: null,
+            type: cc.Prefab
         },
-        popupNode:{
-            default:null,
-            type:cc.Node
+        popupNode: {
+            default: null,
+            type: cc.Node
         },
-        editPhone:{
-            default:null,
-            type:cc.EditBox
-        },
-
-        editCheck:{
-            default:null,
-            type:cc.EditBox
+        editPhone: {
+            default: null,
+            type: cc.EditBox
         },
 
-        editName:{
-            default:null,
-            type:cc.EditBox
+        editCheck: {
+            default: null,
+            type: cc.EditBox
         },
 
-        editPassword:{
-            default:null,
-            type:cc.EditBox
+        editPassword: {
+            default: null,
+            type: cc.EditBox
         },
 
-        editAnpassword:{
-            default:null,
-            type:cc.EditBox
+        editAnpassword: {
+            default: null,
+            type: cc.EditBox
         },
 
-        checkBtn:{
-            default:null,
-            type:cc.Button
+        checkBtn: {
+            default: null,
+            type: cc.Button
         },
 
-        doneBtn:{
-            default:null,
-            type:cc.Button
+        doneBtn: {
+            default: null,
+            type: cc.Button
         },
 
-        loginBtn:{
-            default:null,
-            type:cc.Button
+        loginBtn: {
+            default: null,
+            type: cc.Button
         },
 
-        time:{
+        time: {
+            default: null,
+            type: cc.Label,
+        },
+        misstips:{
             default:null,
-            type:cc.Label,
+            type:cc.Label
         },
 
-        checkCount:null,
-        allPhone:null,
-        selectAcount:null,
+        checkCount: null,
+        allPhone: null,
+        selectAcount: null,
 
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
+    onLoad() {
         this.allPhone = {};
         this.selectAcount = {};
     },
 
-    start () {
+    start() {
         popupManager.init(this);
         this.checkCount = 0;
     },
 
     // update (dt) {},
-//phone
-    onPhoneEditDidBegan: function(editbox, customEventData) {
-    },
+    //phone
+    onPhoneEditDidBegan: function (editbox, customEventData) {},
     //假设这个回调是给 editingDidEnded 事件的
-    onPhoneEditDidEnded: function(editbox, customEventData) {
-    },
+    onPhoneEditDidEnded: function (editbox, customEventData) {},
     //假设这个回调是给 textChanged 事件的
-    onPhoneTextChanged: function(text, editbox, customEventData) {
+    onPhoneTextChanged: function (text, editbox, customEventData) {
         var phone = editbox.string;
-        if(phone.length == 11){
-            console.log('------>this.allPhone:', this.allPhone);
-            var one = this.allPhone[''+phone];
-            if(!one){
-                //联网获取数据
-                var params = {
-                    phone: phone
-                }
-                loginModel.repLrole(params, this.requestLrole, this );
-            }
-        }
     },
 
-    requestLrole(data){
-        var list = data.list;
-        if(list.length>0){
-            var one = list[0];
-            var _id = one._id;
-            var arr = _id.split("@");
-            this.allPhone[arr[1]] = data.list;
-            console.log('------>this.allPhone:', this.allPhone);
-        }
-
-    },
 
     //假设这个回调是给 editingReturn 事件的
-    onPhoneEditingReturn: function(editbox,  customEventData) {
-    },
+    onPhoneEditingReturn: function (editbox, customEventData) {},
 
 
     //Check验证码
-    onCheckEditDidBegan: function(editbox, customEventData) {
-    },
-    onCheckEditDidEnded: function(editbox, customEventData) {
-    },
-    onCheckTextChanged: function(text, editbox, customEventData) {
-        editbox.string=editbox.string.replace(/[^\w\/]/ig,'');//英文数字
+    onCheckEditDidBegan: function (editbox, customEventData) {},
+    onCheckEditDidEnded: function (editbox, customEventData) {},
+    onCheckTextChanged: function (text, editbox, customEventData) {
+        editbox.string = editbox.string.replace(/[^\w\/]/ig, ''); //英文数字
         console.log('------>text.string: ', text.string);
         console.log('------>editbox.string: ', editbox.string);
     },
-    
-    onCheckEditingReturn: function(editbox,  customEventData) {
+
+    onCheckEditingReturn: function (editbox, customEventData) {
 
 
     },
 
-        //nickname
-    onNameEditDidBegan: function(editbox, customEventData) {
+    //nickname
+    onNameEditDidBegan: function (editbox, customEventData) {
 
 
     },
 
-    onNameEditDidEnded: function(editbox, customEventData) {
+    onNameEditDidEnded: function (editbox, customEventData) {
 
     },
 
-    onNameTextChanged: function(text, editbox, customEventData) {
+    onNameTextChanged: function (text, editbox, customEventData) {
 
 
     },
-    
-    onNameEditingReturn: function(editbox,  customEventData) {
+
+    onNameEditingReturn: function (editbox, customEventData) {
 
 
     },
 
     //password
-    onPasswordEditDidBegan: function(editbox, customEventData) {
+    onPasswordEditDidBegan: function (editbox, customEventData) {
 
 
     },
 
-    onPasswordEditDidEnded: function(editbox, customEventData) {
+    onPasswordEditDidEnded: function (editbox, customEventData) {
 
     },
 
-    onPasswordTextChanged: function(text, editbox, customEventData) {
-        editbox.string=editbox.string.replace(/[^\w\/]/ig,'');//英文数字
+    onPasswordTextChanged: function (text, editbox, customEventData) {
+        editbox.string = editbox.string.replace(/[^\w\/]/ig, ''); //英文数字
 
     },
-    
-    onPasswordEditingReturn: function(editbox,  customEventData) {
+
+    onPasswordEditingReturn: function (editbox, customEventData) {
 
 
     },
 
     //重新输入密码
-    onAnPasswordEditDidBegan: function(editbox, customEventData) {
+    onAnPasswordEditDidBegan: function (editbox, customEventData) {
 
 
     },
 
-    onAnPasswordEditDidEnded: function(editbox, customEventData) {
+    onAnPasswordEditDidEnded: function (editbox, customEventData) {
 
     },
 
-    onAnPasswordTextChanged: function(text, editbox, customEventData) {
-        editbox.string=editbox.string.replace(/[^\w\/]/ig,'');//英文数字
-
-    },
-    
-    onAnPasswordEditingReturn: function(editbox,  customEventData) {
-
+    onAnPasswordTextChanged: function (text, editbox, customEventData) {
+        editbox.string = editbox.string.replace(/[^\w\/]/ig, ''); //英文数字
 
     },
 
+    onAnPasswordEditingReturn: function (editbox, customEventData) {
 
 
-    onCheckBtton: function(btn) {
+    },
+
+
+
+    onCheckBtton: function (btn) {
         var phone = this.editPhone.string;
-        if(phone.length != 11){
+        if (phone.length != 11) {
             cc.log('手机号不够11位 ');
-        }else{
+        } else {
             RetrievePasswordModel.repSMS(phone, this.requestGetSMS, this);
             this.lockCheckBtn();
         }
     },
 
-    checkBtnCount(d){
+    checkBtnCount(d) {
         var lable = this.checkBtn.node.getChildByName('title');
         var title = lable.getComponent(cc.Label);
         // cc.log('------->title: ', title);
-        if(this.checkCount == 0){
+        if (this.checkCount == 0) {
             this.unschedule(this.checkBtnCount);
             this.checkBtn.enabled = true;
             title.string = '获取验证码';
             return;
         }
         this.checkCount--;
-        title.string = ''+this.checkCount;
+        title.string = '' + this.checkCount;
         // cc.log('------->lable.string: ', lable.string);
 
     },
-    lockCheckBtn(){
+    lockCheckBtn() {
         this.checkBtn.enabled = false;
         this.checkCount = 120;
         this.schedule(this.checkBtnCount, 1);
     },
 
-    requestGetSMS(data){
+    requestGetSMS(data) {
         cc.log('---->requestGetSMS: ', data);
     },
 
-    onDownmenuBtton: function(btn) {
-        var phone = this.editPhone.string;
-        if(phone.length != 11){
-            cc.log('手机号不够11位 ');
-            return;
+    // onDownmenuBtton: function(btn) {
+    //     var phone = this.editPhone.string;
+    //     if(phone.length != 11){
+    //         cc.log('手机号不够11位 ');
+    //         return;
+    //     }
+
+    //     var list = this.allPhone[phone];
+
+    //     var conf = {
+    //         fun:this.onTouchNameItem,
+    //         target:this,
+    //         data:list,
+    //     };
+    //     popupManager.create('namelist', conf);
+
+    // },
+
+    // onTouchNameItem(arg){
+    //     cc.log('[RetrievePasswoed]  onTouchNameItem: ',arg);
+
+    //     this.editName.string = arg['nick'] || '-------';
+    //     this.selectAcount = arg;
+    // },
+
+    showTIPS(){
+        var self = this;
+        self.misstips.enabled = true;
+        if(self.timerout != -1){
+            clearTimeout(self.timerout);
+            self.timerout = -1;
         }
+        self.timerout = setTimeout(function() {
+            self.misstips.enabled = false;
 
-        var list = this.allPhone[phone];
+        }, 2000);
 
-        var conf = {
-            fun:this.onTouchNameItem,
-            target:this,
-            data:list,
-        };
-        popupManager.create('namelist', conf);
-
-    },
-
-    onTouchNameItem(arg){
-        cc.log('[RetrievePasswoed]  onTouchNameItem: ',arg);
-
-        this.editName.string = arg['nick'] || '-------';
-        this.selectAcount = arg;
-        // this.autoInputPassword(this.editPhone.string, arg);
+        
     },
 
     onDoneBtton(btn) {
@@ -266,47 +252,56 @@ cc.Class({
         var code = this.editCheck.string;
         var password = this.editPassword.string;
         var anpassword = this.editAnpassword.string;
-        var invite = this.selectAcount.invite;
+        // var invite = this.selectAcount.invite;
 
-        if(phone.length != 11){//11
+        if (phone.length != 11) { //11
             cc.log('手机号不够11位 ');
+            this.showTIPS();
             return;
         }
-        if(password==anpassword ){
-            if(password==null || password==''){
+        if (password == anpassword) {
+            if (password == null || password == '') {
                 cc.log('密码不一致');
+                this.showTIPS();
                 return;
             }
-            if(password<6 || password>10){
+            if (password < 6 || password > 10) {
                 cc.log('密码长度6-10');
+                this.showTIPS();
                 return;
             }
-        }else{
+        } else {
             cc.log('password and anpassword  is not same');
+            this.showTIPS();
             return;
         }
 
-         var pp = {
-            phone:phone,
+        var pp = {
+            phone: phone,
             code: code,
-            invite: invite,
-            password:password,
-         };
+            // invite: invite,
+            password: password,
+        };
 
-         RetrievePasswordModel.repRetrievePassword(pp, this.requestDone, this);
-    
+        RetrievePasswordModel.repRetrievePassword(pp, this.requestDone, this);
+
     },
 
-    requestDone (data){
+    requestDone(data) {
         cc.log('---->requestDone: ', data);
-        
+        //保存登录密码。
+        var lastPhone = this.editPhone.string;
+        var passWord = this.editPassword.string;
+        userMode.getInstance().setLastPhone(lastPhone);
+        userMode.getInstance().saveLoginPassWord(passWord);
+
         cc.director.loadScene('Login');
 
     },
 
-    onGoLoginSenceBtton: function(btn) {
+    onGoLoginSenceBtton: function (btn) {
         cc.director.loadScene('Login');
-    
+
     },
 
 });
